3921534327ac857801f831bd1c369adb94d2cb5b
Joako <joaquin.consoni@trocafone.com>
2017-05-26 16:45:54 -0300

    mensajes de news

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,23 @@ var productionPerSecond = 0;
 var geometricScale = 1.2;
 
 var robots = [
-    {type: "first", baseCost: 50, quantity: 0, production: 1}
+    {type: "first", baseCost: 50, quantity: 0, production: 1},
+    {type: "newsletters", baseCost: 500, quantity: 0, production: 10}
+];
+
+var news = [
+    "Sufren las ventas en trocafone, un desarrollador no identificado introdujo un bug crítico en el sistema y se tomó el palo a ‘elementum’",
+    "Fuentes no confirmadas anuncian al ‘Cookie Clickers’ como fuente de inspiración de otros juegos del género",
+    "Confirman que se vendió humo, las acciones de empandas tatú sufren fuertes bajas",
+    "Empleado de Trocafone ingresa a libro Guinness luego de ingerir una cantidad inhumana de empanadas",
+    "Los empleados de Trocafone están pagando la universidad de mis hijos - Máximo Togni, CEO de Dogg.",
+    "Federico Freire, hermano del fundador de Trocafone, se declara capáz de comer 20 empanadas en 20 minutos. Nicolás Díaz, actual campeón de la categoría se mantiene escéptico",
+    "Cookie Clicker demanda a Troca Clicker por plagio. Altos giles",
+    "Siempre que juego al Troca Clicker ligo el ancho de espadas - Gonzalo Higuaín",
+    "Oferta millonaria de Facebook para comprar Troca Clicker. Trocafone dice que no y le descuentan 20 puntos a Nueva Chicago",
+    "Troca Clicker eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee - Diego A. Maradona",
+    "Gracias a Trocafone me compré un Galaxy Pocket Neo a dos mangos - Donald Trump en su discurso de asunción",
+    "Yo no manejo el rating, yo manejo un Rolls-Royce y juego al Troca Clicker - Ricardo Fort",
 ];
 
 function sellPhones(number){
@@ -34,6 +50,12 @@ window.setInterval(function(){
     sellPhones(productionPerSecond);
     document.getElementById('production').innerHTML = productionPerSecond;
 }, 1000);
+
+window.setInterval(function(){
+    var item = news[Math.floor(Math.random()*news.length)];
+    document.getElementById('news').innerHTML = item;
+}, 15000);
+
 
 function recalculateProduction(){
     productionPerSecond = 0;
