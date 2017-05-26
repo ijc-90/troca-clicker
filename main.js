821882f25bfd93cb9817ea1af2383dd203feb083
Joako <joaquin.consoni@trocafone.com>
2017-05-26 15:47:30 -0300

    refactor to make robots an array

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
 var money = 0;
 var cellphoneSellPrice = 10;
-var sellRobotQuantity = 0;
+var productionPerSecond = 0;
+var geometricScale = 1.2;
 
-function sellClick(number){
+var robots = [
+    {type: "first", baseCost: 50, quantity: 0, production: 1}
+];
+
+function sellPhones(number){
     money = money + (number*cellphoneSellPrice);
     document.getElementById("money").innerHTML = money;
-};
-
-function addSellRobot(type){
-    switch(type) {
-    case 1:
-        sellRobotQuantity = sellRobotQuantity + 1;
-        break;
-    default:
-    }
-};
+}
 
 
-function buySellRobot(){
-    var sellRobotCost = Math.floor(10 * Math.pow(1.1,sellRobotQuantity));
-    if(money >= sellRobotCost){
-        sellRobotQuantity = sellRobotQuantity + 1;
-        money = money - sellRobotCost;
-        document.getElementById('sellRobotQuantity').innerHTML = sellRobotQuantity;
+function buyRobot(type){
+    var robot = robots.find(function(robot){
+        return robot.type == type;
+    });
+    var cost = Math.floor(robot.baseCost * Math.pow(geometricScale,robot.quantity));
+    if(money >= cost){
+        robot.quantity += 1;
+        money = money - cost;
+        document.getElementById(robot.type.concat("-quantity")).innerHTML = robot.quantity;
         document.getElementById('money').innerHTML = money;
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,sellRobotQuantity));
-    document.getElementById('robotCost').innerHTML = nextCost;
-};
+    }
+    var nextCost = Math.floor(robot.baseCost * Math.pow(geometricScale,robot.quantity));
+    document.getElementById(robot.type.concat("-cost")).innerHTML = nextCost;
+}
 
 
 window.setInterval(function(){
-    sellClick(sellRobotQuantity);
+    recalculateProduction();
+    sellPhones(productionPerSecond);
+    document.getElementById('production').innerHTML = productionPerSecond;
 }, 1000);
+
+function recalculateProduction(){
+    productionPerSecond = 0;
+    robots.forEach(function(robot){
+        productionPerSecond += robot.production*robot.quantity;
+    });
+    
+}
