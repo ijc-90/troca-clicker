dafc0360fe1613739d7bd51eaf21e68ec150e8f3
Joako <ijc-90@hotmail.com>
2018-09-07 17:21:44 -0300

    integration with frontend adding multiplier for upgrades

--- src/js/view_integrator.js
+++ src/js/view_integrator.js
@@ -36,6 +36,7 @@ class ViewIntegrator {
           "js-amount-phones-awaiting-repair": newContext.stockToRepair, //stock de "en reparo"
           "js-phone-repair-capability": repairRobotsProductivity, //Capacidad de todos los robots de reparo (cantidad * productividad)
           "js-phone-repair-price": newContext.cellphoneRepairPrice,
+          "js-buyer-one-upg-multiplier": 1200,
           "js-sale-balance": newContext.moneyEarnedSellingThisCicle, // cantidad de celulares vendidos * precio de venta
           "js-amount-phones-sold": newContext.amountOfPhonesSoldThisCicle, // cantidad de vendidos
           "js-amount-phones-awaiting-sale": newContext.stockToSale, //stock de venta
@@ -47,6 +48,7 @@ class ViewIntegrator {
           "js-buyer-two-price": 77777777, //idem de acá para abajo
           "js-buyer-two-bought": 77777777,
           "js-buyer-two-upg-price": 77777777,
+          "js-buyer-two-upg-multiplier": 1200,
           "js-buyer-two-total-capacity": 77777777,
           "js-buyer-three-price": 77777777,
           "js-buyer-three-bought": 77777777,
@@ -55,26 +57,32 @@ class ViewIntegrator {
           "js-repairer-one-price": 77777777,
           "js-repairer-one-bought": 77777777,
           "js-repairer-one-upg-price": 77777777,
+          "js-repairer-one-upg-multiplier": 77777777,
           "js-repairer-one-total-capacity": 77777777,
           "js-repairer-two-price": 77777777,
           "js-repairer-two-bought": 77777777,
           "js-repairer-two-upg-price": 77777777,
+          "js-repairer-two-upg-multiplier": 77777777,
           "js-repairer-two-total-capacity": 77777777,
           "js-repairer-three-price": 77777777,
           "js-repairer-three-bought": 77777777,
           "js-repairer-three-upg-price": 77777777,
+          "js-repairer-three-upg-multiplier": 77777777,
           "js-repairer-three-total-capacity": 77777777,
           "js-seller-one-price": 77777777,
           "js-seller-one-bought": 77777777,
           "js-seller-one-upg-price": 77777777,
+          "js-seller-one-upg-multiplier": 77777777,
           "js-seller-one-total-capacity": 77777777,
           "js-seller-two-price": 77777777,
           "js-seller-two-bought": 77777777,
           "js-seller-two-upg-price": 77777777,
+          "js-seller-two-upg-multiplier": 77777777,
           "js-seller-two-total-capacity": 77777777,
           "js-seller-three-price": 77777777,
           "js-seller-three-bought": 77777777,
           "js-seller-three-upg-price": 77777777,
+          "js-seller-three-upg-multiplier": 77777777,
           "js-seller-three-total-capacity": 77777777
         };
     }
