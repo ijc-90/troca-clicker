43ca2ce30ee6b36195182f27e46348a726833a41
Joako <ijc-90@hotmail.com>
2018-09-12 00:39:06 -0300

    test

--- src/js/game_loop.js
+++ src/js/game_loop.js
@@ -1,118 +1,117 @@
 class GameLoop {
 
-	constructor(){
-		this.viewIntegrator = new ViewIntegrator();
+    constructor(){
+        this.viewIntegrator = new ViewIntegrator();
 
-		this.context = {
-			ticksPerSecond: 1,
-			money : 500,
-			cellphoneBuyPrice : 50,
-			cellphoneRepairPrice : 40,
-			cellphoneSalePrice : 100,
-			geometricCostScale : 1.3,
-			robots : {
-				"buyer_one": { name: "buyer_one", type: "buy", baseCost: 120, quantity: 0, production: 1, salary: 20 },
-				"buyer_two": { name: "buyer_two", type: "buy", baseCost: 1200, quantity: 0, production: 5, salary: 40 },
-				"buyer_three": { name: "buyer_three", type: "buy", baseCost: 12000, quantity: 0, production: 25, salary: 120 },
+        this.context = {
+            ticksPerSecond: 1,
+            money : 300,
+            cellphoneBuyPrice : 50,
+            cellphoneRepairPrice : 40,
+            cellphoneSalePrice : 100,
+            geometricCostScale : 1.3,
+            robots : {
+                "buyer_one": { name: "buyer_one", type: "buy", baseCost: 150, quantity: 0, production: 1, salary: 120 },
+                "buyer_two": { name: "buyer_two", type: "buy", baseCost: 10000, quantity: 0, production: 100, salary: 8000 },
+                "buyer_three": { name: "buyer_three", type: "buy", baseCost: 11000000, quantity: 0, production: 8000, salary: 8800000 },
 
-				"repairer_one" : { name: "repairer_one", type: "repair", baseCost: 120, quantity: 0, production: 1, salary: 60 },
-				"repairer_two" : { name: "repairer_two", type: "repair", baseCost: 1200, quantity: 0, production: 5, salary: 120 },
-				"repairer_three" : { name: "repairer_three", type: "repair", baseCost: 12000, quantity: 0, production: 25, salary: 360 },
+                "repairer_one" : { name: "repairer_one", type: "repair", baseCost: 150, quantity: 0, production: 1, salary: 120 },
+                "repairer_two" : { name: "repairer_two", type: "repair", baseCost: 10000, quantity: 0, production: 70, salary: 8000 },
+                "repairer_three" : { name: "repairer_three", type: "repair", baseCost: 11000000, quantity: 0, production: 5600, salary: 8800000 },
 
-				"seller_one" : { name: "seller_one", type: "sale", baseCost: 120, quantity: 0, production: 1, salary: 20 },
-				"seller_two" : { name: "seller_two", type: "sale", baseCost: 1200, quantity: 0, production: 5, salary: 40 },
-				"seller_three" : { name: "seller_three", type: "sale", baseCost: 12000, quantity: 0, production: 25, salary: 120 },
-			},
-			upgrades: {
-				"upgrade_buyer_one": { price: 2000, productionMultiplier: 2, robot: "buyer_one", isActive: false },
-				"upgrade_buyer_two": { price: 5000, productionMultiplier: 3, robot: "buyer_two", isActive: false },
-				"upgrade_buyer_three": { price: 100000, productionMultiplier: 4, robot: "buyer_three", isActive: false },
+                "seller_one" : { name: "seller_one", type: "sale", baseCost: 150, quantity: 0, production: 1, salary: 120 },
+                "seller_two" : { name: "seller_two", type: "sale", baseCost: 10000, quantity: 0, production: 100, salary: 8000 },
+                "seller_three" : { name: "seller_three", type: "sale", baseCost: 11000000, quantity: 0, production: 8000, salary: 8800000 },
+            },
+            upgrades: {
+                "upgrade_buyer_one": { price: 2000, productionMultiplier: 2, robot: "buyer_one", isActive: false },
+                "upgrade_buyer_two": { price: 140000, productionMultiplier: 3, robot: "buyer_two", isActive: false },
+                "upgrade_buyer_three": { price: 150000000, productionMultiplier: 4, robot: "buyer_three", isActive: false },
 
-				"upgrade_repairer_one": { price: 2000, productionMultiplier: 2, robot: "repairer_one", isActive: false },
-				"upgrade_repairer_two": { price: 5000, productionMultiplier: 3, robot: "repairer_two", isActive: false },
-				"upgrade_repairer_three": { price: 100000, productionMultiplier: 4, robot: "repairer_three", isActive: false },
+                "upgrade_repairer_one": { price: 2000, productionMultiplier: 2, robot: "repairer_one", isActive: false },
+                "upgrade_repairer_two": { price: 140000, productionMultiplier: 3, robot: "repairer_two", isActive: false },
+                "upgrade_repairer_three": { price: 150000000, productionMultiplier: 4, robot: "repairer_three", isActive: false },
 
-				"upgrade_seller_one": { price: 2000, productionMultiplier: 2, robot: "seller_one", isActive: false },
-				"upgrade_seller_two": { price: 5000, productionMultiplier: 3, robot: "seller_two", isActive: false },
-				"upgrade_seller_three": { price: 100000, productionMultiplier: 4, robot: "seller_three", isActive: false },
-			},
-			stockToRepair: 0,
-			stockToSale: 0,
-			events: [],
-			iterationsToPaySalary: 60,
-			gameOver: false,
-			gameWon: false,
-			probabilityOfDefectiveBuy : 0.7,
+                "upgrade_seller_one": { price: 2000, productionMultiplier: 2, robot: "seller_one", isActive: false },
+                "upgrade_seller_two": { price: 140000, productionMultiplier: 3, robot: "seller_two", isActive: false },
+                "upgrade_seller_three": { price: 150000000, productionMultiplier: 4, robot: "seller_three", isActive: false },
+            },
+            stockToRepair: 0,
+            stockToSale: 0,
+            events: [],
+            iterationsToPaySalary: 60,
+            gameOver: false,
+            gameWon: false,
+            probabilityOfDefectiveBuy : 0.7,
 
-			moneySpentOnBuyingPhonesThisCicle: 0,
-			amountOfPhonesBoughtThisCicle: 0,
-			moneySpentRepairingThisCicle: 0,
-			amountOfPhonesRepairedThisCicle: 0,
-			moneyEarnedSellingThisCicle: 0,
-			amountOfPhonesSoldThisCicle: 0,
-			moneyPaidInSalaries: 0,
-			salariesPaidThisTick: false,
+            moneySpentOnBuyingPhonesThisCicle: 0,
+            amountOfPhonesBoughtThisCicle: 0,
+            moneySpentRepairingThisCicle: 0,
+            amountOfPhonesRepairedThisCicle: 0,
+            moneyEarnedSellingThisCicle: 0,
+            amountOfPhonesSoldThisCicle: 0,
+            moneyPaidInSalaries: 0,
+            salariesPaidThisTick: false,
 
-		};
-		
-		this.pipeline = [
-			new SaleFlow(),
-			new RepairFlow(),
-			new BuyFlow(),
-			new HiringFlow(),
-			new UpgradeFlow(),
-			new SalaryFlow(),
-		];
+        };
+        
+        this.pipeline = [
+            new RepairFlow(),
+            new BuyFlow(),
+            new SaleFlow(),
+            new HiringFlow(),
+            new UpgradeFlow(),
+            new SalaryFlow(),
+        ];
 
-		this.events = [];
-	}
+        this.events = [];
+    }
 
-	tick(ticksPerSecond) {
-		this.context.ticksPerSecond = ticksPerSecond;
-		if (this.context.gameOver){
-			return;
-		}
-		var oldContext = JSON.parse(JSON.stringify(this.context));
-
-		this.context.events = this.events;
-		this.events = [];
-
-		this.context = this.pipeline.reduce( function(context, flowStep){
-			return flowStep.work(context);
-		}, this.context);
-
-		return this.viewIntegrator.generateViewRawData(oldContext, this.context);
-	}
-
-	buyClick() {
-		this.events.push(new Event(EVENTS.BUY_CLICK));
-	}
-
-	repairClick() {
-		this.events.push(EVENTS.REPAIR_CLICK);
-	}
-
-	saleClick() {
-		this.events.push(new Event(EVENTS.SALE_CLICK));
-	}
-
-	hiringClick(robotName){
-	    if ($("#".robotName).hasClass("disabled")) {
-	        return;
+    tick() {
+        if (this.context.gameOver){
+            return;
         }
-		var event = new Event(EVENTS.HIRING_CLICK);
-		event.robotName = robotName;
-		this.events.push(event);
-	}
+        var oldContext = JSON.parse(JSON.stringify(this.context));
 
-	upgradeClick(upgradeName){
+        this.context.events = this.events;
+        this.events = [];
+
+        this.context = this.pipeline.reduce( function(context, flowStep){
+            return flowStep.work(context);
+        }, this.context);
+
+        return this.viewIntegrator.generateViewRawData(oldContext, this.context);
+    }
+
+    buyClick() {
+        this.events.push(new Event(EVENTS.BUY_CLICK));
+    }
+
+    repairClick() {
+        this.events.push(EVENTS.REPAIR_CLICK);
+    }
+
+    saleClick() {
+        this.events.push(new Event(EVENTS.SALE_CLICK));
+    }
+
+    hiringClick(robotName){
         if ($("#".robotName).hasClass("disabled")) {
             return;
         }
-		var event = new Event(EVENTS.UPGRADE_CLICK);
-		event.upgradeName = upgradeName;
-		this.events.push(event);
-	}
+        var event = new Event(EVENTS.HIRING_CLICK);
+        event.robotName = robotName;
+        this.events.push(event);
+    }
+
+    upgradeClick(upgradeName){
+        if ($("#".robotName).hasClass("disabled")) {
+            return;
+        }
+        var event = new Event(EVENTS.UPGRADE_CLICK);
+        event.upgradeName = upgradeName;
+        this.events.push(event);
+    }
 
 
 
